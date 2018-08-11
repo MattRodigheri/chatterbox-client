9ff76ab71547d71bc8e817e97585a2dec38cbbb3
MattRodigheri <mrodigheri@gmail.com>
2018-08-11 12:01:38 -0700

    Completed basic requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,6 +9,9 @@ $('document').ready(function() {
 
 app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
 app.storage = [];
+app.user = {};
+app.user.friends = {};
+app.user.currentRoom;
 app.rooms = [];
 
 app.init = function(data) {
@@ -19,7 +22,8 @@ app.init = function(data) {
     $('.roomInput').val('');
   });
   app.fetch();
-  setTimeout(app.refreshFeed, 200);
+  app.addEventListeners();
+  app.user.currentRoom = 'selectRoom';
 };
 
 app.send = function(message) {
@@ -41,9 +45,11 @@ app.fetch = function() {
   $.ajax({
     url: app.server,
     type: 'GET',
+    data: 'order=-createdAt',
     success: function (data) {
       console.log('chatterbox: Message recieved');
       app.storage = data;
+      app.refreshFeed(data);
     },
     error: function (data) {
       console.error('chatterbox: Failed to recieve message', data);
@@ -56,11 +62,24 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
+  var scriptTag = RegExp('script');
+  if (scriptTag.test(message.text) || scriptTag.test(message.username)) { 
+    return; 
+  }
+
+  if (app.user.currentRoom !== message.roomname && app.user.currentRoom !== 'selectRoom') {
+    return;
+  }
+
   var usernameNode = $(`<span class="username"> ${message.username} </span>`);
-  var textNode = $(`<span> <p> ${message.text} </p> </span>`);
+
+  if (app.user.friends.hasOwnProperty(message.username)) {
+    usernameNode = $(`<span class="username"> <b> ${message.username} </b> </span>`);
+  }
+
+  var textNode = $(`<span> <p> ${message.text} ${message.createdAt} </p> </span>`);
   $('#chats').append(usernameNode);
   usernameNode.append(textNode);
-  app.send(message);
   return usernameNode;
 };
 
@@ -69,18 +88,20 @@ app.renderRoom = function(room) {
   $('#roomSelect').append(node);
 };
 
-app.handleUsernameClick = function() {
-  console.log('test');
+app.handleUsernameClick = function(message) {
+  app.user.friends[message.username] = 1;
+  app.fetch();
 };
 
 app.submitMessage = function() {
   var username = $('.usernameInput').val();
   var text = $('.messageInput').val();
   var date = new Date();
-  var messageObj = {username: username, text: text};
-  app.renderMessage(messageObj);
+  var messageObj = {username: username, text: text, createdAt: date, roomname: app.user.currentRoom};
+  app.send(messageObj);
   $('.usernameInput').val('');
   $('.messageInput').val('');
+  app.fetch();
 };
 
 app.handleSubmit = function() {
@@ -90,20 +111,29 @@ app.addEventListeners = function() {
   $('.submit').on('click', function() {
     app.submitMessage();
     app.handleSubmit();
+    console.log('submit');
+  });
+  $('.rooms').on('change', function(event) {
+    app.user.currentRoom = $(this).val();
+    app.fetch();
   });
 };
 
 app.refreshFeed = function(messages) {
+  app.clearMessages();
   var messages = app.storage.results;
   messages.forEach(function(message) {
     var node = app.renderMessage(message);
-    node.click(app.handleUsernameClick);
+    if (node) { 
+      node.click(app.handleUsernameClick.bind(app, message));
+    }
   });
   app.createRoomsFromMessages();
 };
 
 app.createRoomsFromMessages = function() {
   var messages = app.storage.results;
+  app.rooms.push('selectRoom');
   messages.forEach(function(message) {
     if (message.hasOwnProperty('roomname')) {
       if (message.roomname !== '' && !app.rooms.includes(message.roomname)) {
@@ -114,7 +144,7 @@ app.createRoomsFromMessages = function() {
 }
 
 app.createRoom = function(roomName) {
-  var node = `<option value="">${roomName}</option>`
+  var node = `<option value="${roomName}" class="${roomName}">${roomName}</option>`
   $('.rooms').append(node);
   app.rooms.push(roomName); 
 }
